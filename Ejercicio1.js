bbd217ff9b92147f4492ab300b5fe37f40c840af
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-09-10 21:03:36 -0300

    ADD Ejercicio variables y tipo de datos

---
+++ Ejercicio1.js
@@ -0,0 +1,16 @@
+
+/*Ejercicio variable y tipo de datos*/ 
+
+const nombre = "Diego";
+
+const edad = 18;
+
+const puedoConducir = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(puedoConducir);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
