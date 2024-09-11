7a082fc38c9e0fa6c686f57a55b702d875e08ed0
Diego-E36 <diego.gonzalez2101@alumnos.ubiobio.cl>
2024-09-11 02:20:20 -0300

    ADD Ejercicio 8 objetos

---
+++ Ejercicio8.js
@@ -0,0 +1,29 @@
+
+const persona = {
+    nombre: "Diego",
+    edad: 21,
+    genero : "Hombre"
+}
+
+console.log(persona);
+
+const caja = {
+    cuaderno: 10,
+    lapices: 15,
+    papel: 100,
+    fotografias: 25,
+    buenEstado: true
+}
+
+console.log(caja);
+console.log(caja.cuaderno);
+console.log(caja.lapices);
+console.log(caja.papel);
+console.log(caja.fotografias);
+console.log(caja.buenEstado);
+console.log(typeof caja)
+console.log(typeof caja.cuaderno);
+console.log(typeof caja.lapices);
+console.log(typeof caja.papel);
+console.log(typeof caja.fotografias);
+console.log(typeof caja.buenEstado);
